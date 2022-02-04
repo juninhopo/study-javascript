2239d0728254da968dffa57155ecb0c4e29d58d6
juninhopo <kelgaxd@gmail.com>
2022-02-03 22:35:24 -0300

    update task2

---
+++ task2.js
@@ -0,0 +1,19 @@
+console.log("trabalhando com variaveis")
+//js e case sensitive
+
+idade = 29;
+Idade = 50;
+
+console.log(idade)
+console.log(Idade)
+
+const idade2 = 34;
+const name = "darlan";
+
+console.log(idade2);
+console.log(name);
+
+let ano = 2020;
+
+console.log(ano);
+
