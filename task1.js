35ae5b9c777b896fb0042d5c92fdbfe6eb24d969
juninhopo <kelgaxd@gmail.com>
2022-02-03 22:23:52 -0300

    update task1

--- task1.js
+++ task1.js
@@ -2,4 +2,12 @@ console.log("Task1 - Trabalhando com variaveis");
 
 const idade = 23;
 
+console.log("idade");
 console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+console.log(idade * 2);
+
+const idadeSomada = idade + 5;
+console.log(idadeSomada);
