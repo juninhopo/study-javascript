7974c02c2441d53442dd54bf6ca089f951920449
juninhopo <kelgaxd@gmail.com>
2022-02-03 23:02:33 -0300

    update task4

---
+++ task4.js
@@ -0,0 +1,12 @@
+console.log("Conversao de Tipos");
+
+console.log("ano" + 2022);
+console.log("2" + "2");
+
+console.log(parseInt("2") + parseInt("4"));
+
+console.log("10" / "2");
+console.log("Ricardo" / "2"); //Not a number = NaN
+
+console.log(6.5);
+console.log(6,5);
