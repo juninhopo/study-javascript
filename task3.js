7ec616c5ab18a99a0909319eb1b5b14c24b93a9b
juninhopo <kelgaxd@gmail.com>
2022-02-03 22:55:25 -0300

    update task3

---
+++ task3.js
@@ -0,0 +1,10 @@
+console.log("Operacoes aritimeticas");
+
+
+console.log(2 + 2);
+console.log(10 + 9 - 2 / 2);
+console.log((((10 + 9) - 2) /2));
+
+console.log("2" + "2");
+console.log("ano" + 2022);
+
