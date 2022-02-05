dd87a6120f77fa54671e692ca8f604465c526944
juninhopo <kelgaxd@gmail.com>
2022-02-05 01:51:49 -0300

    update

--- banking-js/ContaCorrente.js
+++ banking-js/ContaCorrente.js
@@ -1,19 +1,40 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
     agencia;
-    cliente;
+    _cliente;
     _saldo = 0;
 
-    sacar(valor){
-        if (this._saldo >= valor){
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+    }
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
             this._saldo -= valor;
             return valor;
         }
     }
 
-    depositar(valor){
-        if (valor > 0) {
+    depositar(valor) {
+        if (valor <= 0) {
             return;
-        }   
+        }
         this._saldo += valor;
     }
 
--- banking-js/index.js
+++ banking-js/index.js
@@ -1,24 +1,14 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 8882223339;
+const cliente2 = new Cliente("Alice", 8882223339);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
-contaCorrenteRicardo.depositar = (500);
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+const conta2 = new ContaCorrente(102, cliente2);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
-contaCorrenteRicardo.transferir(200, conta2);
-
-console.log(contaCorrenteRicardo);
-console.log(conta2);
